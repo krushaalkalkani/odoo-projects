1b5ecac0b893628b106a69537e150b05b5cd6675
Krushal Kalkani <krushal.kalkani78@gmial.com>
2023-12-06 11:52:47 +0530

    Aura Project Added

---
+++ aura/__manifest__.py
@@ -0,0 +1,13 @@
+
+{
+    'name': 'Aura',
+    'version': '1.1',
+    'category': 'Apps',
+    'summary': 'Sales Teams - Marketing',
+    'author': 'Krushal Kalkani',
+    'data': [
+        "security/ir.model.access.csv",
+        "views/aura.xml"],
+    'license': 'LGPL-3',
+
+}
