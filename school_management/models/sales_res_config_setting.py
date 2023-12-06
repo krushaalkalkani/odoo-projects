086397edbf55f5ce5b24ce0409ebd0b82e495b1b
Krushal Kalkani <krushal.kalkani78@gmial.com>
2023-12-06 11:51:40 +0530

    Modify aura Project: to add in the github

--- school_management/models/sales_res_config_setting.py
+++ school_management/models/sales_res_config_setting.py
@@ -5,3 +5,4 @@ class ConfSetting(models.TransientModel):
     _inherit = "res.config.settings"
     discount_limit = fields.Boolean(
         string="Discount Limit", store=True, config_parameter="school_management.discount_limit")
+#  hello 
