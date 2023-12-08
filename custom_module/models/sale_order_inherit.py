4635c2f312eda8f3852355b7ae670e0cca408fd4
Krushal Kalkani <krushal.kalkani78@gmial.com>
2023-12-08 16:51:12 +0530

    Updated the all two field while click on confirm button

--- custom_module/models/sale_order_inherit.py
+++ custom_module/models/sale_order_inherit.py
@@ -27,20 +27,13 @@ class SaleOrder(models.Model):
             },
         }
 
-    def action_confirm_with_vehicle_data(self):
+    def action_confirm(self):
         res = super(SaleOrder, self).action_confirm()
-        for order in self:
-            picking = order.picking_ids.filtered(lambda p: p.state == 'draft')
-            if picking:
-                picking.write({
-                    'vehicle_part_number': order.vehicle_part_number or "",
-                    'vehicle_chassis_number': order.vehicle_chassis_number or "",
-                })
-            else:
-                self.env['stock.picking'].create({
-                    'sale_id': order.id,
-                    'vehicle_part_number': order.vehicle_part_number or "",
-                    'vehicle_chassis_number': order.vehicle_chassis_number or "",
-                })
+
+        for picking in self.picking_ids:
+            picking.write({
+                'vehicle_part_number': self.vehicle_part_number,
+                'vehicle_chassis_number': self.vehicle_chassis_number,
+            })
 
         return res
